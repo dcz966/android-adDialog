4c24475b1d5e8ec6318eeb3f8bfcc28f06b3050b
duchengzhen <dcz966@163.com>
2017-02-10 13:42:21 +0800

    remove nineoldandroids

--- adlibrary/src/main/java/com/uuch/adlibrary/transformer/RotateDownPageTransformer.java
+++ adlibrary/src/main/java/com/uuch/adlibrary/transformer/RotateDownPageTransformer.java
@@ -1,10 +1,10 @@
 package com.uuch.adlibrary.transformer;
 
 import android.annotation.SuppressLint;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
-import com.nineoldandroids.view.ViewHelper;
 
 /**
  * http://blog.csdn.net/lmj623565791/article/details/40411921
@@ -25,22 +25,22 @@ public class RotateDownPageTransformer implements ViewPager.PageTransformer {
 
         if (position < -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
-            ViewHelper.setRotation(view, 0);
+            ViewCompat.setRotation(view, 0);
         } else if (position <= 1) // a页滑动至b页 ； a页从 0.0 ~ -1 ；b页从1 ~ 0.0
         { // [-1,1]
             // Modify the default slide transition to shrink the page as well
             if (position < 0) {
 
                 mRot = (ROT_MAX * position);
-                ViewHelper.setPivotX(view, view.getMeasuredWidth() * 0.5f);
-                ViewHelper.setPivotY(view, view.getMeasuredHeight());
-                ViewHelper.setRotation(view, mRot);
+                ViewCompat.setPivotX(view, view.getMeasuredWidth() * 0.5f);
+                ViewCompat.setPivotY(view, view.getMeasuredHeight());
+                ViewCompat.setRotation(view, mRot);
             } else {
 
                 mRot = (ROT_MAX * position);
-                ViewHelper.setPivotX(view, view.getMeasuredWidth() * 0.5f);
-                ViewHelper.setPivotY(view, view.getMeasuredHeight());
-                ViewHelper.setRotation(view, mRot);
+                ViewCompat.setPivotX(view, view.getMeasuredWidth() * 0.5f);
+                ViewCompat.setPivotY(view, view.getMeasuredHeight());
+                ViewCompat.setRotation(view, mRot);
             }
 
             // Scale the page down (between MIN_SCALE and 1)
@@ -49,7 +49,7 @@ public class RotateDownPageTransformer implements ViewPager.PageTransformer {
 
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
-            ViewHelper.setRotation(view, 0);
+            ViewCompat.setRotation(view, 0);
         }
     }
 }
